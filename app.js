ad39d5c3cbc0c841d56abbd37bbea3f7aa7afd11
mijalromano <mijalmsr@gmail.com>
2022-08-18 16:04:35 -0300

    home

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const app = express();
 
 app.use(express.static('public'))
 
-app.listen(3001, function(){
-    console.log('🚀escuchando en el puerto 3001');
+app.listen(3001, () =>{
+    console.log('🚀servidor escuchando en el puerto 3001');
 });
 
-app.get('/', function (req, res) {
+app.get('/',  (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
+
+
+
+
+
+

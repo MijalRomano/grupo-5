886b544c2588e2ffc3dee84bfdaaa3d22568b866
EstySrugo <estelasrugo@gmail.com>
2022-08-23 19:35:55 -0300

    nuevos cambios

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ app.get('/',  (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
+app.get('/register', function(req, res){
+
+    res.sendFile(path.join(__dirname,  '/views/register.html'));
+})
+
+app.get('/login', function(req, res){
+
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+})
 
 
 

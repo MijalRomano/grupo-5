35774de0ba5cb53a547c13203799eb6af9b4366e
mijalromano <mijalmsr@gmail.com>
2022-08-29 19:08:46 -0300

    rutas

---
+++ routes/mainRuters.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const controller = {
+    
+main: (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/index.html"))
+}
+
+
+}
+
+module.exports = controller;

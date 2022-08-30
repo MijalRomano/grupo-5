5f541c9c6b6a7d97acc127648b08fa62387e20eb
EstySrugo <estelasrugo@gmail.com>
2022-08-30 18:14:09 -0300

    cambios

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require('path');
-let rutasProductos = require ("./routes/productsRuters.js");
-let rutasMin = require ("./routes/main.js");
 const app = express();
 
 app.use(express.static('public'));
@@ -32,20 +30,6 @@ app.listen(3001, (req, res) => {
     console.log('Servidor escuchando en el puerto 3001');
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/login'));
-});
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/register'));
-});
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/productCart'));
-});
-
-app.use(express.static('public'));
-app.use('/main', routerMain);
-app.use('/user', routerUser);
-app.use('/products', routerProducts);
 

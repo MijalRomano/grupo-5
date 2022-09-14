1718ed233148de5300e816b3b49aaef9eb78a5b2
mijalromano <mijalmsr@gmail.com>
2022-09-14 11:18:30 -0300

    rutas

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -4,11 +4,11 @@ const mainController = require('../controllers/mainController');
 
 router.get('/delete', mainController.delete);
 router.get('/edit', mainController.edit);
-<<<<<<< HEAD
+
 router.get('/Usercreate', mainController.create);
-=======
+
 router.get('/create', mainController.create);
->>>>>>> 12bcaf6e7d048a0f215b7020b1fbd7c5f8d9af90
+
 router.get('/productCart', mainController.productCart);
 router.get('/login', mainController.login);
 router.get('/', mainController.index);

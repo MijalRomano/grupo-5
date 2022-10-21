a917d9350361be493874f7f28a744bab364f953c
mijalmaleh <mijalmaleh@gmail.com>
2022-10-21 18:34:29 -0300

    avance un poquito

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,38 +2,41 @@
 const fs = require('fs');
 const path = require('path');
 const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator')
 
 
-/*let todosLosUsuarios= usuarios;*/
-
-/*
-findByeField=function(field,text){ 
-    let usuarioBuscado= todosLosUsuarios.find(oneUser=> oneUser[field]===text);
-    return usuarioBuscado;
-}
-*/
 const mainController = {
     login: (req, res) => {
         return res.render('login');
     },
 
     loginProcess: (req, res) => {
-        const userData = req.body;
-        const usersJSON = fs.readFileSync(path.join(__dirname, "../data/usersDB.json"), "utf-8");
-        const usuarios = JSON.parse(usersJSON);
+        let errores = validationResult(req);
+        if (errores.isEmpty()) {
 
-        const usuarioLogueado = usuarios.find(thisUser => thisUser.email === userData.email);
-        if (usuarioLogueado) {
-            let contraseñaCorrecta = bcryptjs.compareSync(userData.contrasenia, usuarioLogueado.contrasenia);
-            if (contraseñaCorrecta) {
-                /*res.send("bienvenido");*/
-                res.redirect ('/');
-            } else {
-               /* res.send("el email o la contraseña no coinciden")*/
-               res.redirect('login');
+            const userData = req.body;
+            const usersJSON = fs.readFileSync(path.join(__dirname, "../data/usersDB.json"), "utf-8");
+            const usuarios = JSON.parse(usersJSON);
+
+            const usuarioLogueado = usuarios.find(thisUser => thisUser.email === userData.email);
+            if (usuarioLogueado) {
+                let contraseñaCorrecta = bcryptjs.compareSync(userData.contrasenia, usuarioLogueado.contrasenia);
+                if (contraseñaCorrecta) {
+                    /*res.send("bienvenido");*/
+                    res.redirect('/');
+                } else {
+                    /* res.send("el email o la contraseña no coinciden")*/
+                    res.redirect('login');
+                }
             }
+        } else {
+            /*res.send("hay errores")*/
+
+            //para enviar los errores a la vista, los agrego a la misma, con una propiedad que la nombre error, donde pido 
+            // que mi variable errores se muestre como array.
+            res.render('login', {error:errores.array()});
         }
-        /*
+        /****la forma que uso nacho en clasee 
         const usuarioLogueado = usuarios.find(thisUser => thisUser.email ===userData.email);
         const contraseñaCorrecta= bcryptjs.compareSync(userData.contrasenia, usuarioLogueado.contrasenia);
         console.log(contraseñaCorrecta);
@@ -47,8 +50,8 @@ const mainController = {
 
         /*console.log(usuarios);
         console.log(usuarioLogueado);*/
-        console.log(usuarioLogueado.contrasenia, userData.contrasenia);
-
+  /*      console.log(usuarioLogueado.contrasenia, userData.contrasenia);
+*/
 
 
     },
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { body }= require ('express-validator');
+const { body } = require('express-validator');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
 
@@ -14,17 +14,21 @@ router.get('/productCart', mainController.productCart);
 router.get('/productDetail', mainController.productDetail);
 */
 
-const loginValidaciones=[
- body('nombre').notEmpty().withMessage('Este campo es obligatorio'),
- body ('email').notEmpty().withMessage ('Este campo es obligatorio') .bail()
-               . isEmail().withMessage('Por favor ingrese un email válido'),
- body ('contrasenia') .notEmpty().withMessage('Este campo es obligatorio') .bail()
-                       .isLength({min:3}).withMessage('La contraseña no es correcta'),
+
+//validaciones para el login
+const loginValidaciones = [
+    body('nombre').notEmpty().withMessage('Este campo es obligatorio'),
+    body('email').notEmpty().withMessage('Este campo es obligatorio').bail()
+        .isEmail().withMessage('Por favor ingrese un email válido'),
+    body('contrasenia').notEmpty().withMessage('Este campo es obligatorio').bail()
+        .isLength({ min: 2 }).withMessage('La contraseña no es correcta'),
 ]
 
 
+
+//en el post del login implementamos la constante de validaciones como middelware 
 router.get('/login', mainController.login);
-router.post('/login', loginValidaciones, mainController.loginProcess);
+router.post('/login',loginValidaciones, mainController.loginProcess);
 
 
 
@@ -33,8 +37,9 @@ router.get('/', mainController.index);
 router.get('/user', mainController.user); //que seria?
 /*router.get('/register', mainController.register); */
 router.post('/user/users', mainController.Postregister);// que seria?
-router.get("/asesoramiento", mainController.asesoramiento); 
+router.get("/asesoramiento", mainController.asesoramiento);
 router.get('/error', mainController.error);
 router.get('/ayuda', mainController.ayuda);
+
 module.exports = router;
 

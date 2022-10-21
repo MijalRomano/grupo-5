cf94ba21536631f0f370edcbf2492c0392ff500a
mijalmaleh <mijalmaleh@gmail.com>
2022-10-21 01:55:30 -0300

    primera parte express validator en login

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { body }= require ('express-validator');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
 
-router.get('/delete', mainController.Userdelete);
+/*router.get('/delete', mainController.Userdelete);
 router.get('/edit', mainController.Useredit);
 
 router.get('/Usercreate', mainController.Usercreate);
@@ -11,14 +12,27 @@ router.get('/create', mainController.Usercreate);
 
 router.get('/productCart', mainController.productCart);
 router.get('/productDetail', mainController.productDetail);
+*/
+
+const loginValidaciones=[
+ body('nombre').notEmpty().withMessage('Este campo es obligatorio'),
+ body ('email').notEmpty().withMessage ('Este campo es obligatorio') .bail()
+               . isEmail().withMessage('Por favor ingrese un email válido'),
+ body ('contrasenia') .notEmpty().withMessage('Este campo es obligatorio') .bail()
+                       .isLength({min:3}).withMessage('La contraseña no es correcta'),
+]
+
 
 router.get('/login', mainController.login);
-router.post('/login', mainController.loginProcess);
+router.post('/login', loginValidaciones, mainController.loginProcess);
+
+
+
 
 router.get('/', mainController.index);
-router.get('/user', mainController.user);
-router.get('/register', mainController.register);
-router.post('/user/users', mainController.Postregister);
+router.get('/user', mainController.user); //que seria?
+/*router.get('/register', mainController.register); */
+router.post('/user/users', mainController.Postregister);// que seria?
 router.get("/asesoramiento", mainController.asesoramiento); 
 router.get('/error', mainController.error);
 router.get('/ayuda', mainController.ayuda);

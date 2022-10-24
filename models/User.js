69e4019763cca8ac40c949744ea19d3ad9e2280e
mijalromano <mijalmsr@gmail.com>
2022-10-23 23:29:48 -0300

    login completo

--- models/User.js
+++ models/User.js
@@ -1,18 +1,13 @@
-//1. Guardar al usuario en la DB.json.
-//2. Buscar al usuario que se quiere loguear por su email.
-//3. Buscar a un usuario por su ID.4
-//4. Editar la informacion de un usuario.
-//5. Eliminar a un usuario de la DB.
 const fs = require('fs');
 const User = {
-    fileName:'./dataBase/usersDB.json',
+    fileName:'./data/usersDB.json',
     
     getData: function () {
-        return fs.readFileSync(this.fileName, 'utf-8');
+        return JSON.parse (fs.readFileSync(this.fileName, 'utf-8'));
     },
 
     create: function (userData){
     }
 }
 
-console.log(User.getData);
+console.log(User.getData());

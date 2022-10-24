4b0cd06904f6a20df5ff31ecbd9c9a055e36b0f5
mijalromano <mijalmsr@gmail.com>
2022-10-24 16:39:06 -0300

    login completo

--- models/User.js
+++ models/User.js
@@ -1,13 +1,40 @@
 const fs = require('fs');
+const path = require('path');
 const User = {
-    fileName:'./data/usersDB.json',
-    
+    fileName: '../src/data/usersDB.json',
+
     getData: function () {
-        return JSON.parse (fs.readFileSync(this.fileName, 'utf-8'));
+        return JSON.parse(fs.readFileSync(path.join(__dirname, this.fileName), 'utf-8'));
     },
 
-    create: function (userData){
+generateId: function () {
+   let allUsers = this.findAll();
+   let lastUser = allUsers.pop();
+ return lastUser=id + 1;
+},
+
+
+    findAll: function name() {
+        return this.getData();
+    },
+
+    findByPk: function (id) {
+        let allUsers = this.getData();
+        let userFound = allUsers.find(oneUser => oneUser.id === id);
+        return userFound;
+    },
+
+    findField: function (field, text) {
+        let allUsers = this.getData();
+        let userFound = allUsers.find(oneUser => oneUser[field] === text);
+        return userFound;
+    },
+
+    create: function (userData) {
+     let allUsers = this.getData();
+    allUsers.push(userData);
+    fs.writeFileSync(this.fileName, JSON.stringify(allUsers, null, ' '));
+    return true;
     }
 }
-
-console.log(User.getData());
+console.log(User.generateId());

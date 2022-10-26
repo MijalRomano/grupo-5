483899ddb0cb457e2856d4df8e61b55b16a28b16
mijalmaleh <mijalmaleh@gmail.com>
2022-10-25 23:36:41 -0300

    ultima version q habia, pero no funciona

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -18,7 +18,7 @@ const mainController = {
             const usersJSON = fs.readFileSync(path.join(__dirname, "../data/usersDB.json"), "utf-8");
             const usuarios = JSON.parse(usersJSON);
 
-            /*const nombreUsuario= usuarios.find(thisUser => thisUser.nombre === userData.nombre);*/
+            const nombreUsuario= usuarios.find(thisUser => thisUser.nombre === userData.nombre);
             const usuarioALoguear = usuarios.find(thisUser => thisUser.email === userData.email);
             if (usuarioALoguear ) {
                 let contraseñaCorrecta = bcryptjs.compareSync(userData.contrasenia, usuarioALoguear.contrasenia);
@@ -121,7 +121,6 @@ const mainController = {
 
 
 module.exports = mainController;
-
 
 
 
